d18e2ed3811e594a8741fb53630bd3abefa17cee
Lucas <Jack@BlackPearl.io>
2019-08-20 15:49:44 -0400

    configured enzyme and shifted dependencies

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, ShallowWrapper } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  let subject: ShallowWrapper;
+
+  beforeEach(() => {
+    subject = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should display the happy sock image', () => {
+    expect(subject.find('.App-logo').exists()).toBeTruthy();
+  });
 });
