dc9175fd231350f594db3fae2d9e0c330edee534
Destini Barcelona <destinibarcelona@yahoo.com>
2019-12-12 13:35:55 -0500

    Fix date formatter for web client

--- src/main/java/dgs1sdt/pie/rfis/WebGetsClient.java
+++ src/main/java/dgs1sdt/pie/rfis/WebGetsClient.java
@@ -3,25 +3,21 @@ package dgs1sdt.pie.rfis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 public class WebGetsClient implements GetsClient {
   @Value("${GETS_REQUEST_TIME_FRAME_IN_DAYS}")
   private int requestDays;
 
   private static String calculateDateStringDaysBeforeNow(int days) {
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-    LocalDate date = LocalDate.now().minusDays(days);
-    return dateFormat.format(date);
+    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd");
+    LocalDate localDate = LocalDate.now().minusDays(days);
+    return dateFormat.format(localDate);
   }
 
   @Override
